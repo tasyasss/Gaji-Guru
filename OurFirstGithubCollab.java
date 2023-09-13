2ccce93618910a53994629bc525810d9f24c98e2
tasyasss <tasyasakina0703@gmail.com>
2023-09-13 11:48:21 +0700

    commit kedua 11.48

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -3,5 +3,6 @@ package GajiGuru;
 public class OurFirstGithubCollab {
     public static void main(String[] args){
         System.out.println("ini commit pertama di collab github");
+        System.out.println("coba kembangkan fitur pertama, commit kedua");
     }
 }
